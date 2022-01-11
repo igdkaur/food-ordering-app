0b9703f779d2a57f25d7fb9289a66990a80e4f89
igdkaur <igdkaur@gmail.com>
2022-01-11 20:43:25 +0530

    cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import classes from './Header.module.css'
 import mealsImg from '../../assets/meals.jpg' // .jpg rqrd
+import HeaderCartButton from './HeaderCartButton'
 
 
 const Header = () => {
+  
   return (
-    <div>
-      <header>
+    <>
+      <header className={classes.header}>
         <h2> Wine & Dine </h2>
+        <HeaderCartButton/>
       </header>
-      <div>
+      <div className={classes['main-image']}>
         <img src = {mealsImg} alt= "delicious food"/> 
       </div>
-    </div>
+    </>
   )
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import CartIcon from '../Cart/CartIcon'
+import classes from  './HeaderCartButton.module.css'
 
 const HeaderCartButton = () => {
   return (
-    <div>
-      
-    </div>
+    <button className = {classes.button}>
+      <span className = {classes.icon} > <CartIcon/> </span>
+      <span > Your Cart  </span>
+      <span className = {classes.badge}> 3 </span>      
+    </button>
   )
 }
 
