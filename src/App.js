0f88af5e850c2701866d3c6a0134ff2cd0521807
igdkaur <igdkaur@gmail.com>
2022-01-12 20:11:19 +0530

    dont show cart bydefault

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+
+const [showCart,setShowCart] = useState(false);
+
+
   return (
     <>
-    <Cart/>
+    {showCart && <Cart/>}
       <Header />
       <main>
         <Meals />
@@ -17,3 +21,8 @@ function App() {
 }
 
 export default App;
+
+// open cart only when your cart clicked
+
+// close by default -
+// render conditionally
