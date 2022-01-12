3b0855533fc3c28d9cfe5405514e886ae0d16bdd
igdkaur <igdkaur@gmail.com>
2022-01-12 17:38:14 +0530

    wrap cart component with modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -17,11 +17,12 @@ const ModalOverlay = (props) => {
   )
 }
 
+const portalElement = document.getElementById('overlays')
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />)}
-      {ReactDOM.createPortal(<ModalOverlay> {props.children} </ModalOverlay> )}
+      {ReactDOM.createPortal(<Backdrop />, portalElement )}
+      {ReactDOM.createPortal(<ModalOverlay> {props.children} </ModalOverlay>, portalElement )}
     </Fragment>
   )
 }
