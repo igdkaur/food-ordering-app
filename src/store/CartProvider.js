21e69c704f5f96f6642a272585f86fceda4e02fa
igdkaur <igdkaur@gmail.com>
2022-01-13 18:18:26 +0530

    manage data in context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,6 +2,22 @@ import React from 'react'
 import CartContext from './cart-context'
 
 const CartProvider = (props) => {
+
+const addItemToCartHandler = () => {
+    
+}
+
+const RemoveItemFromCartHandler = () => {
+    
+}
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: RemoveItemFromCartHandler
+  }
+
   return (
     <CartContext.Provider>
       {props.children}
