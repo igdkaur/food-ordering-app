d3e75b9b5708cc8473da2aab5c9b952ba297be36
igdkaur <igdkaur@gmail.com>
2022-01-17 15:02:13 +0530

    bump feature

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,40 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
-import { useContext } from 'react';
+import { useContext,useState, useEffect} from 'react';
 
 const HeaderCartButton = (props) => {
 
+  
   const cartCtx = useContext(CartContext); 
   const cartItemsCount = cartCtx.items.reduce((cn,item) => {
     return cn + item.amount
   },0);
 
+  const {items} = cartCtx;
+
+  const [btnIsHigh, setBtnIsHigh] = useState(false)
+  useEffect(() => {
+    if(items.length === 0) {
+      return;
+    }
+
+    setBtnIsHigh(true);
+
+    setTimeout(()=> {
+      setBtnIsHigh(false);
+    }, 300)
+
+    // return () => {
+    //   cleanup
+    // }
+  }, [items])
+
+
+  const btnClasses = `${classes.button} ${btnIsHigh ? classes.bump : ''}`
+
   return (
-    <button className={classes.button} onClick = {props.onClick}>
+    <button className={btnClasses} onClick = {props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
